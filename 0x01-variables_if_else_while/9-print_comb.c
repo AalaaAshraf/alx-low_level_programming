e16694b2d0240c331cdd2fe7a293133bd04dcaa1
AalaaAshraf <aalaaashraf1102001@gmail.com>
2023-03-20 16:15:59 -0700

    ll

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ *main - entry point
+ *
+ * description: print 0-9 using putchar
+ *              while using int variable
+ *
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
