0cb67d4dc8988d3bdc1bc4ebb5633b835b070fd1
AalaaAshraf <aalaaashraf1102001@gmail.com>
2023-04-25 15:27:18 -0700

    aa

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,9 +4,8 @@
  * main - prints the name of the file
  *
  * Return: Always 0 (Success)
- * description ain print function
  */
-int ain(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
